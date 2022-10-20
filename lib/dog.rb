ae1d40a03d46b9241825ea061b84f96ff691224d
Evans Ochieng <evansochieng121@gmail.com>
2022-10-20 20:52:27 +0300

    create setter and getter methods

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
+    #setter methods
+    def name=(name)
+        @name = name
+    end
 
+    def breed=(breed)
+        @breed = breed
+    end
+
+    #getter methods
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
 class Person
+    #setter methods
+    def name=(name)
+        @name = name
+    end
 
+    def job= (job)
+        @job = job
+    end
+
+    #getter methods
+    def name
+        @name
+    end
+
+    def job
+        @job
+    end
 end
